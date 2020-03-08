93bf8b465c5820e284c50a015bdada234e11adeb
pgorgis <pgorgis@kth.se>
2020-03-08 21:51:28 +0100

    Active teacher highlighted with red, fixed id bug

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ class App extends Component {
             </div>
             <div className="flex-2">
               {this.state.active_teacher === null ? null : <TeacherDetails active_teacher={this.state.active_teacher} active_course={this.state.active_course} courseIdUpdate={newValue => this.courseIdUpdate(newValue)}/>}
-              {this.state.active_course === null ? null : <CourseDetails active_course={this.state.active_course} teacherIdUpdate={newValue => this.teacherIdUpdate(newValue)} />}
+              {this.state.active_course === null ? null : <CourseDetails active_course={this.state.active_course} active_teacher={this.state.active_teacher} teacherIdUpdate={newValue => this.teacherIdUpdate(newValue)} />}
             </div>
           </div>
         </div>
